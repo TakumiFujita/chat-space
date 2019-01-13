5dbc6926144778cd46878cdd326d8d84a8b6608c
TakumiFujita <takumi@takumi-no-MacBook-Pro.local>
2019-01-13 21:45:58 +0900

    edit ajax2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,7 @@ class MessagesController < ApplicationController
       format.html { redirect_to group_messages_path(@group),notice: 'メッセージが送信されました' }
       format.json
     end
-    # redirect_to group_messages_path(@group),notice: 'メッセージが送信されました'
+
   else
     @message = @group.messages.includes(:user)
     flash.now[:alert] = 'メッセージを入力してください。'
