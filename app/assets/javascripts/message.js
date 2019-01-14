7e7998c3da55f00c30bb50680e83c3c107725b23
TakumiFujita <takumi@takumi-no-MacBook-Pro.local>
2019-01-14 15:42:21 +0900

    edit ajax 5

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){
   function buildHTML(message){
+    if (message.image) {
     var html = `
     <div class = "main-content_body_list-message">
       <div class = "main-content_body_list-message-name">
@@ -10,10 +11,25 @@ $(function(){
       </div>
       <div class = "main-content_body_list-message-text">
       <p>${message.content}</p>
+      <img src="${message.image}">
       </div>
     </div>`
     return html;
   }
+
+    else {
+    var html = `
+      <div class = "main-content_body_list-message-name">
+      ${message.user_name}
+      </div>
+      <div class = "main-content_body_list-message-time">
+      ${message.created_at}
+      </div>
+      <div class = "main-content_body_list-message-text">
+      <p>${message.content}</p>`
+    return html;
+  }
+}
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -28,14 +44,17 @@ $(function(){
         })
     .done(function(data){
       var html = buildHTML(data);
-      console.log($('.main-content_body_list')[0]);
       $('.main-content_body_list').append(html);
       $('.main-content_footer_body_message-area').val('')
-      $('.main-content_body_list').animate({scrollTop: $('.main-content_body_list')[0].scrollHeight}, 'fast');
+      $('#upload-icon').val('');
+      // $('file')[0].reset();
       $(".main-content_footer_body_submit-btn-area").prop("disabled", false);
+      $('.main-content_body_list').animate({scrollTop: $('.main-content_body_list')[0].scrollHeight}, 'fast');
     })
     .fail(function(){
+      // $(".main-content_footer_body_submit-btn-area").prop("disabled", false);
       alert('error');
+      $(".main-content_footer_body_submit-btn-area").prop("disabled", false);
     })
   });
 });
